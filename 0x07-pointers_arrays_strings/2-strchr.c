77a7db9341b0f2b407edd830dc35d530d693468d
ndifongroups <ndifonvincent@gmail.com>
2022-09-26 20:41:56 -0700

    beautiful message

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stddef.h>
-
 /**
  * _strchr - locates a character in a string
  * @s: the string
@@ -12,17 +11,12 @@
 char *_strchr(char *s, char c)
 {
 
-	int i;
-
+	int i;	
 	for (i = 0; s[i] != '\0' ; i++)
 	{
 
 		if (s[i] == c)
-
 		return (&s[i]);
-
 	}
-
 	return (0);
-
 }
